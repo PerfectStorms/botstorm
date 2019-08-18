45196fb71576e9fb6a1941bb4caa217ad7303699
egnaf <mamadaliev@yahoo.com>
2019-08-18 15:41:35 +0300

    Add Home controller for test the home page

---
+++ backend/src/main/java/org/perfectstorms/society/controllers/HomeController.java
@@ -0,0 +1,13 @@
+package org.perfectstorms.society.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String index() {
+        return "test";
+    }
+}
