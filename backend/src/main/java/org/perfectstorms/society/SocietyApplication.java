7e2ae399c82209f85715c1b6a58c8618a3e5cb4b
egnaf <mamadaliev@yahoo.com>
2019-08-18 15:40:40 +0300

    Add SocietyApplication.java

---
+++ backend/src/main/java/org/perfectstorms/society/SocietyApplication.java
@@ -0,0 +1,12 @@
+package org.perfectstorms.society;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocietyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SocietyApplication.class, args);
+    }
+}
