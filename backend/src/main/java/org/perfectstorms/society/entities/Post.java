11f4fcbed784027916b1f8ab98cc05c8da15f1c5
egnaf <mamadaliev@yahoo.com>
2019-08-21 01:40:59 +0300

    Add Post entity

---
+++ backend/src/main/java/org/perfectstorms/society/entities/Post.java
@@ -0,0 +1,62 @@
+package org.perfectstorms.society.entities;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "posts")
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "timestamp")
+    private Long timestamp;
+
+    public Post() {
+    }
+
+    public Post(String title, String description, Long timestamp) {
+        this.title = title;
+        this.description = description;
+        this.timestamp = timestamp;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Long getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Long timestamp) {
+        this.timestamp = timestamp;
+    }
+}
