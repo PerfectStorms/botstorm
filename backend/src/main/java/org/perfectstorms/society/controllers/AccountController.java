5a88568b164e41ad175b1f8c904b9f8a630b28c4
egnaf <mamadaliev@yahoo.com>
2019-08-31 22:17:39 +0300

    Fix Account controller * Fix addUser method * Add AddAccount method * Add deleteAccount method

--- backend/src/main/java/org/perfectstorms/society/controllers/AccountController.java
+++ backend/src/main/java/org/perfectstorms/society/controllers/AccountController.java
@@ -3,6 +3,8 @@ package org.perfectstorms.society.controllers;
 import org.perfectstorms.society.entities.Account;
 import org.perfectstorms.society.repositories.AccountRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -23,8 +25,20 @@ public class AccountController {
         return accountRepository.findAll();
     }
 
-    @GetMapping(value = "/user/{id}")
+    @GetMapping(value = "/account/{id}")
     public Optional<Account> getOneById(@PathVariable Long id) {
         return accountRepository.findById(id);
+    }
+
+    @PostMapping(value = "/accounts")
+    public ResponseEntity addAccount(@RequestBody Account account) {
+        accountRepository.save(account);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @RequestMapping(value = "/accounts", method = RequestMethod.DELETE)
+    public ResponseEntity deleteAccountById(Long id) {
+        accountRepository.deleteById(id);
+        return new ResponseEntity(HttpStatus.OK);
     }
 }
