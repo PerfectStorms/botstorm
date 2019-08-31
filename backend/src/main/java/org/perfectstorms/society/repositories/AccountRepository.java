c7075be3473a173772af0c0f7c15425e97d658cb
egnaf <mamadaliev@yahoo.com>
2019-08-31 22:15:04 +0300

    Add Account Squashed commit of the following:
    commit 996e35101ad52b3fbf250b241d24f539194cf5a5
    Author: Sherzod Mamadaliev <mamadaliev@yahoo.com>
    Date:   Sat Aug 31 21:31:26 2019 +0300
    
        Add Account (#6)
    
        * Add Account entity
        * Add Account repository
        * Add Account controller

---
+++ backend/src/main/java/org/perfectstorms/society/repositories/AccountRepository.java
@@ -0,0 +1,9 @@
+package org.perfectstorms.society.repositories;
+
+import org.perfectstorms.society.entities.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AccountRepository extends JpaRepository<Account, Long> {
+}
